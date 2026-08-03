f378769d777ee78cce9ec369fb0a6e3a44df1c9d
alef <alefamaraltads@hotmail.com>
2026-08-03 08:26:17 -0300

    Commit inicial: Base do Spring Boot configurada

---
+++ src/main/java/br/com/valemorar/ValemorarApplication.java
@@ -0,0 +1,13 @@
+package br.com.valemorar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValemorarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValemorarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/valemorar/ValemorarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.valemorar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValemorarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
